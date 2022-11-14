7f94e1ee1c16c571f88addb0221aaa8edd2fb755
Israel Vasquez <ing.visrael@gmail.com>
2022-11-14 15:12:19 -0500

    migration update

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 import { AppRegistry, Platform } from 'react-native';
 import moment from 'moment';
 import App from './App';
-
+import Gleap from 'react-native-gleapsdk';
 import { i18next } from './src/i18n';
 import 'moment-timezone';
 import 'moment/locale/es';
+
+Gleap.initialize('P26aq9PfN8vTJNCG94pBGnLHpiabvUJ5');
 
 const appName = Platform.select({
   ios: () => 'coJobcoreTalent',
